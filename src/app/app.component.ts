6dbbed040ebcbcdd58f2bf20db90eb3a6c4fd6b3
quyetdc <quyetdc.uet@gmail.com>
2020-12-15 06:10:27 +0700

    Communicate parent App component and card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Neat Tree",
+      imageUrl: "assets/images/lion.jpg",
+      username: "nature",
+      content: "Saw this awesome tree during my hike today."
+    },
+    {
+      title: "Reindeer",
+      imageUrl: "assets/images/reindeer.jpg",
+      username: "nature",
+      content: "I wish I can this animal once in my life."
+    },
+    {
+      title: "Cat",
+      imageUrl: "assets/images/cat.jpg",
+      username: "nature",
+      content: "I love this so kute cat."
+    }
+  ]
 }
